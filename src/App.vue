dba5db801372e0b1247ded0d3c142e5a30470187
Fifth <i.m.galazewska@gmail.com>
2026-06-07 13:31:15 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup></script>
+<script setup>
+import Pogoda from './components/Pogoda.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <Pogoda />
+  </main>
 </template>
-
-<style scoped></style>
