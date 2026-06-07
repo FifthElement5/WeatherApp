ef952964d518f415f18a70d45e754eff3a6cb9e6
Fifth <i.m.galazewska@gmail.com>
2026-06-07 13:38:14 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Pogoda from './components/Pogoda.vue'
+import Wyszukiwarka from './components/Wyszukiwarka.vue'
 </script>
 
 <template>
   <main>
     <Pogoda />
+    <Wyszukiwarka /> 
   </main>
 </template>
