76a1579cb75cee7525c7464133c55ae85ef312b8
Fifth <i.m.galazewska@gmail.com>
2026-06-07 13:26:04 +0200

    first

---
+++ src/components/pogoda.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pogoda">
+    <h1>To jest moja pogoda!</h1>
+  </div>
+</template>
+
+<style scoped>
+.pogoda {
+  color: blue;
+  padding: 20px;
+  border: 1px solid black;
+}
+</style>
